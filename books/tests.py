9878357123d65c3d5003cb17d9cfa178149c5e99
Shelly Yusuf <ShellyNYusuf@gmail.com>
2017-06-20 22:45:42 -0700

    adding a test

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Book
+
+class BookTestCase(TestCase):
+    def setUp(self):
+        Book.objects.create(title="mytestbook")
+
+    def test_book_title(self):
+        test = Book.objects.get(title="mytestbook")
+        print(test)
+        self.assertEqual(test.title, 'mytestbook')
